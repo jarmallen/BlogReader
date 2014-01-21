3f33459e5ecf6b85d8ac19da4dc82e612e6371b2
jarmallen <jallen@jallen-mba.local>
2014-01-20 17:23:20 -0800

    Initial Commit

---
+++ BlogReader/JAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JAAppDelegate.h
+//  BlogReader
+//
+//  Created by Jared Allen on 1/20/14.
+//  Copyright (c) 2014 Jared Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader
+//
+//  Created by Jared Allen on 1/20/14.
+//  Copyright (c) 2014 Jared Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JAAppDelegate class]));
+    }
+}
